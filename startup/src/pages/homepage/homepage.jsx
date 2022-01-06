3613e3f13d31d98bc83c0b6fa7b0319c0f674277
GhassanDabak <ghassan.h.dabak@gmail.comgit config --global user.name GhassanDabakgit config --global user.email ghassan.h.dabak@gmail.com>
2022-01-06 19:37:35 +0200

    edited title and title image

--- startup/src/pages/homepage/homepage.jsx
+++ startup/src/pages/homepage/homepage.jsx
@@ -1,19 +1,51 @@
-import React from 'react'
-import FeaturedGames from '../../components/featured-games/featured-games'
-import FeaturedRooms from '../../components/featured-rooms/featured-rooms'
-import HeroImage from '../../components/hero-img/hero-img'
-import Testimonials from '../../components/testimonials/testimonials'
+import React from "react";
+import FeaturedGames from "../../components/featured-games/featured-games";
+import FeaturedRooms from "../../components/featured-rooms/featured-rooms";
+import HeroImage from "../../components/hero-img/hero-img";
+import Testimonials from "../../components/testimonials/testimonials";
+import "./homepage.css";
 
 function HomePage() {
-    return (
-        <div>
-            <HeroImage />
-            <FeaturedGames />
-            <FeaturedRooms />
-            <Testimonials />
-            
+    const sendToLocalStorage = (e)=>{
+        e.preventDefault();
+        document.querySelector('#form').reset();
+    }
+  return (
+    <div>
+      <HeroImage />
+      <FeaturedGames />
+      <FeaturedRooms />
+      <Testimonials />
+      <div className="animated">
+        <h1 className="featured-games-title">Contact me, I won't bite!</h1>
+      </div>
+      <hr />
+      <main class="main-cont-contact">
+        <div class="container-contact">
+          <div class="header-contact">
+            <h2>Sign in</h2>
+          </div>
+          <form id="form" className="form-contact" onSubmit={(e)=>sendToLocalStorage(e)}>
+            <div class="form-control">
+              <label for="username">Name</label>
+              <input type="text" id="username" />
+            </div>
+            <div class="form-control">
+              <label for="text">Email</label>
+              <input type="text" id="text" />
+            </div>
+            <div class="form-control">
+              <label htmlFor="text">Message</label>
+              <input type="text" id="text" />
+            </div>
+            <button type="submit" class="log-btn">
+              Submit
+            </button>
+          </form>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
